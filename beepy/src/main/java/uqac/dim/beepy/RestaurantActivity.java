d61165cceaf8f23f13b9df909c6de14f5957364b
Antoine <24255551+RedSkyi@users.noreply.github.com>
2022-04-11 22:44:38 -0400

    Setup map in RestaurantActivity

--- beepy/src/main/java/uqac/dim/beepy/RestaurantActivity.java
+++ beepy/src/main/java/uqac/dim/beepy/RestaurantActivity.java
@@ -1,55 +1,118 @@
 package uqac.dim.beepy;
 
-import android.content.Intent;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import uqac.dim.beepycommon.models.Restaurant;
-import uqac.dim.beepycommon.utils.FirestoreKeys;
-
-public class RestaurantActivity extends AppCompatActivity {
+public class RestaurantActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private final FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
+    private static final int ZOOM_DEFAULT = 15;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private boolean locationPermissionGranted;
+    private GoogleMap map;
+    private Location lastKnownLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
 
-        findViewById(R.id.restaurant_submit).setOnClickListener(this::onSubmit);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.restaurant_map);
+        if(mapFragment != null) mapFragment.getMapAsync(this);
     }
 
-    private void onSubmit(View v) {
-        String name = ((TextView) findViewById(R.id.restaurant_input)).getText().toString();
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.map = googleMap;
 
-        DocumentReference document = firestore.collection(FirestoreKeys.RESTAURANTS_COLLECTION).document(name);
-        document.get().addOnCompleteListener(task -> {
-            if(task.isSuccessful()) {
-                Restaurant restaurant;
-                DocumentSnapshot snapshot = task.getResult();
+        checkPermissionLocation();
+    }
 
-                if (snapshot.exists()) {
-                    restaurant = task.getResult().toObject(Restaurant.class);
-                } else {
-                    restaurant = new Restaurant();
-                    restaurant.setId(name);
-                    restaurant.setName(name);
+    @SuppressLint("MissingPermission")
+    private void updateLocationUi() {
+        if(map == null) return;
 
-                    document.set(restaurant);
-                }
-
-                Intent intent = new Intent(RestaurantActivity.this, TableActivity.class);
-                intent.putExtra(Restaurant.RESTAURANT_EXTRA, restaurant);
-                startActivity(intent);
-                finish();
+        try{
+            if(locationPermissionGranted) {
+                map.setMyLocationEnabled(true);
+                map.getUiSettings().setMyLocationButtonEnabled(true);
             }
-        });
+        }catch(SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getDeviceLocation() {
+        if(map == null) return;
+
+        try{
+            if(locationPermissionGranted) {
+                Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+                locationTask.addOnCompleteListener(task -> {
+                    if(task.isSuccessful()) {
+                        lastKnownLocation = task.getResult();
+                        if(lastKnownLocation != null) {
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(lastKnownLocation.getLatitude(),
+                                            lastKnownLocation.getLongitude()),
+                                    ZOOM_DEFAULT
+                            ));
+                        }
+                    }
+                });
+            }
+        }catch(SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    private void checkPermissionLocation() {
+        if(ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+            updateLocationUi();
+            getDeviceLocation();
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    new String[]{ Manifest.permission.ACCESS_FINE_LOCATION },
+                    PERMISSION_REQUEST_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == PERMISSION_REQUEST_FINE_LOCATION) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+                updateLocationUi();
+                getDeviceLocation();
+            }
+        }else super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
