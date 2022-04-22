80da9563337a67c68bbfac0e8c7abfecb0d73dbf
Antoine <24255551+RedSkyi@users.noreply.github.com>
2022-04-22 11:53:15 -0400

    Update table selection

--- beepy/src/main/java/uqac/dim/beepy/TableActivity.java
+++ beepy/src/main/java/uqac/dim/beepy/TableActivity.java
@@ -1,16 +1,18 @@
 package uqac.dim.beepy;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import uqac.dim.beepycommon.models.Restaurant;
 import uqac.dim.beepycommon.models.Table;
@@ -36,7 +38,25 @@ public class TableActivity extends AppCompatActivity {
 
         ((TextView) findViewById(R.id.table_restaurant)).setText(restaurant.getName());
 
-        findViewById(R.id.table_submit).setOnClickListener(this::onSubmit);
+        firestore.collection(FirestoreKeys.RESTAURANTS_COLLECTION).document(restaurant.getId())
+                .collection(FirestoreKeys.TABLES_COLLECTION).get().addOnCompleteListener(task -> {
+                    if(task.isSuccessful()) {
+                        LinearLayout layout = findViewById(R.id.table_list);
+                        layout.removeAllViews();
+
+                        LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+                        for(QueryDocumentSnapshot document : task.getResult()) {
+                            Table table = document.toObject(Table.class);
+                            Log.d("AZEAZEAZE", table.getName());
+                            @SuppressLint("InflateParams") View view = inflater.inflate(R.layout.table_element, null, false);
+
+                            ((TextView) view.findViewById(R.id.table_name)).setText(table.getName());
+                            view.findViewById(R.id.table_select).setOnClickListener(v -> onSubmit(table));
+
+                            layout.addView(view);
+                        }
+                    }
+        });
     }
 
     private boolean checkRestaurantExtra() {
@@ -51,32 +71,11 @@ public class TableActivity extends AppCompatActivity {
         return true;
     }
 
-    private void onSubmit(View v) {
-        String name = ((TextView) findViewById(R.id.table_input)).getText().toString();
-
-        DocumentReference document = firestore.collection(FirestoreKeys.RESTAURANTS_COLLECTION)
-                .document(restaurant.getId()).collection(FirestoreKeys.TABLES_COLLECTION).document(name);
-        document.get().addOnCompleteListener(task -> {
-            if(task.isSuccessful()) {
-                Table table;
-                DocumentSnapshot snapshot = task.getResult();
-
-                if(snapshot.exists()) {
-                    table = snapshot.toObject(Table.class);
-                }else{
-                    table = new Table();
-                    table.setId(name);
-                    table.setName(name);
-
-                    document.set(table);
-                }
-
-                Intent intent = new Intent(TableActivity.this, MainActivity.class);
-                intent.putExtra(Restaurant.RESTAURANT_EXTRA, restaurant);
-                intent.putExtra(Table.TABLE_EXTRA, table);
-                startActivity(intent);
-            }
-        });
+    private void onSubmit(Table table) {
+        Intent intent = new Intent(TableActivity.this, MainActivity.class);
+        intent.putExtra(Restaurant.RESTAURANT_EXTRA, restaurant);
+        intent.putExtra(Table.TABLE_EXTRA, table);
+        startActivity(intent);
     }
 
 }
